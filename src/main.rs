2201ee464d74c4f56edd4c416d7ad43d039d6623
afmiguel <afonso.miguel@gmail.com>
2024-10-30 20:27:07 -0300

    Atualizado segunda função

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
-fn primeiro_i32(lista: &Vec<i32>) -> &i32 {
-    lista.get(0).expect("A lista está vazia")
+fn first_i32(list: &Vec<i32>) -> &i32 {
+    list.get(0).expect("The list is empty")
 }
-fn primeiro_string(lista: &Vec<String>) -> &String {
-    lista.get(0).expect("A lista está vazia")
+fn first_string(list: &Vec<String>) -> &String {
+    list.get(0).expect("The list is empty")
 }
 
 fn main() {
-    let numeros = vec![1, 2, 3];
-    let primeiro_numero = primeiro_i32(&numeros);
-    println!("Primeiro número: {:?}", primeiro_numero);
+    let numbers = vec![1, 2, 3];
+    let first_number = first_i32(&numbers);
+    println!("First number: {:?}", first_number);
 
-    let palavras = vec!["olá".to_string(), "mundo".to_string()];
-    let primeira_palavra = primeiro_string(&palavras);
-    println!("Primeira palavra: {:?}", primeira_palavra);
+    let words = vec!["hello".to_string(), "world".to_string()];
+    let first_word = first_string(&words);
+    println!("First word: {:?}", first_word);
 }
